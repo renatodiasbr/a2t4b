1dff5e59739101d0730e480dd7d9d15e43cbd2d8
Renato Gonçalves Dias <renatodias.SPASSU@petrobras.com.br>
2017-09-01 15:43:07 -0300

    password.validator.ts

--- src/app/change-password/password.validators.ts
+++ src/app/change-password/password.validators.ts
@@ -45,18 +45,6 @@ export class PasswordValidators {
     };
   }
 
-  static passwordsShouldMatch2(newPasswordPath, confirmPasswordPath): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const newPassWord = control.get(newPasswordPath);
-      const confirmPassWord = control.get(confirmPasswordPath);
-      if (newPassWord != null && confirmPassWord != null) {
-        return newPassWord.value !== confirmPassWord.value ? { passwordsShouldMatch: true } : null;
-      }
-
-      return null;
-    };
-  }
-
   static passwordsShouldMatch(control: AbstractControl): ValidationErrors | null {
       const newPassWord = control.get('newPassword');
       const confirmPassWord = control.get('confirmPassword');
